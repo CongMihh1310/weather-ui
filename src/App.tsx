88a7445e916bc23ac8ea3a9f313ce94fb987daa3
MinhNC <10a11ngominh@gmail.com>
2025-06-06 10:37:57 +0700

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { WeatherForecast } from "./types";
+import "./App.css";
 
 function App() {
+  const [forecasts, setForecasts] = useState<WeatherForecast[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/WeatherForecast`)
+      .then((response) => response.json())
+      .then((data) => {
+        setForecasts(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Forecast</h1>
+      <table>
+        <thead>
+          {" "}
+          {forecasts.length < 1 ? (
+            <tr>
+              <h1>No data available</h1>
+            </tr>
+          ) : (
+            <tr>
+              <th>Date</th>
+              <th>Temp. (C)</th>
+              <th>Temp. (F)</th>
+              <th>Summary</th>
+            </tr>
+          )}
+        </thead>
+        <tbody>
+          {forecasts.map((forecast, index) => (
+            <tr key={index}>
+              <td>{new Date(forecast.date).toLocaleDateString()}</td>
+              <td>{forecast.temperatureC}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
